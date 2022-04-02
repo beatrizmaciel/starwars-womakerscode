c91c6bd8caa9c6da2f1ccd5022b5d37ecf9d6afc
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-04-02 16:17:42 -0300

    first commit

---
+++ src/main/java/com/bibs/starwars/StarWarsBootcampWomakersCodeApplication.java
@@ -0,0 +1,13 @@
+package com.bibs.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsBootcampWomakersCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsBootcampWomakersCodeApplication.class, args);
+	}
+
+}
