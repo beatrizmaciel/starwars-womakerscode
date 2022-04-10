a82b8dc96b9f96f90d7679ace6dc50e48acc3131
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-04-10 18:55:11 -0300

    implementação de métodos no Service

--- src/main/java/com/bibs/starwars/service/JediService.java
+++ src/main/java/com/bibs/starwars/service/JediService.java
@@ -1,8 +1,13 @@
 package com.bibs.starwars.service;
 
+import com.bibs.starwars.model.Jedi;
+import com.bibs.starwars.repository.JediRepositoryImpl;
 import org.springframework.stereotype.Service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Optional;
 
 // para reconhecer que é um serviço: @Service
 @Service
@@ -12,9 +17,44 @@ public class JediService {
     private static final Logger logger = LogManager.getLogger(JediService.class);
 
     // repository
+    private final JediRepositoryImpl jediRepositoryImpl;
 
-    public void a(){
-        logger.info("{}Log do método", "qualquer coisa");
+    // construtor do repository:
+    public JediService(JediRepositoryImpl jediRepositoryImpl) {
+        this.jediRepositoryImpl = jediRepositoryImpl;
     }
 
+    public Optional<Jedi> findById(int id){
+        logger.info("Find Jedi with id: {}", id);
+        return jediRepositoryImpl.findById(id);
+    }
+
+    public List<Jedi> findAll(){
+        logger.info("Bring all Jedis from the Galaxy");
+
+        return jediRepositoryImpl.findAll();
+    }
+
+    public Jedi save(Jedi jedi){
+        jedi.setVersion("1");
+
+        logger.info("Update Jedi from system");
+
+        return jediRepositoryImpl.save(jedi);
+    }
+
+    public boolean update(Jedi jedi){
+        boolean updated = false;
+
+        Jedi savedJedi = this.save(jedi);
+        if (savedJedi != null){
+            updated = true;
+        }
+
+        return updated;
+    }
+
+    public boolean delete(int id){
+        return true;
+    }
 }
