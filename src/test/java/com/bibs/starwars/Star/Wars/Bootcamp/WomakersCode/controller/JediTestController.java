796a21a33d63d6d3e752511884040045d04553c4
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-04-14 22:18:30 -0300

    creating successfull test at JediTestController

--- src/test/java/com/bibs/starwars/Star/Wars/Bootcamp/WomakersCode/controller/JediTestController.java
+++ src/test/java/com/bibs/starwars/Star/Wars/Bootcamp/WomakersCode/controller/JediTestController.java
@@ -1,15 +1,32 @@
 package com.bibs.starwars.Star.Wars.Bootcamp.WomakersCode.controller;
 
+import com.bibs.starwars.model.Jedi;
 import com.bibs.starwars.service.JediService;
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+
+
+import java.util.Optional;
 
 
 @ExtendWith(SpringExtension.class)
@@ -22,6 +39,31 @@ public class JediTestController {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("GET /jedi/1 - SUCCESS")
+    public void testGetJediByIdFoundSuccessfully() throws Exception{
+
+        // cenario
+        Jedi mockJedi = new Jedi(1, "HanSolo", 10, 1);
+        Mockito.doReturn(Optional.of(mockJedi)).when(jediService).findById(1);
+
+        // execução
+        mockMvc.perform(get("/jedi/{id}",1))
+                // asserts
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(header().string(HttpHeaders.ETAG,"\"1\""))
+                .andExpect(header().string(HttpHeaders.LOCATION,"/jedi/1"))
+
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("HanSolo")))
+                .andExpect(jsonPath("$.strength", is(10)))
+                .andExpect(jsonPath("$.version", is(1)));
+
+
+    }
 
     static String asJsonString(final Object obj) {
         try {
